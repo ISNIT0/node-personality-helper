85a11837894276b052c5731c1cee9a7050ac97b3
Joseph Reeve <joseph.reeve@googlemail.com>
2017-10-05 13:59:09 +0100

    INIT

---
+++ lib.js
@@ -0,0 +1,18 @@
+const request = require('request-promise-native');
+
+module.exports = function makeMessageResponder(config, language, personality) {
+    return function getMessageReponse(message, sourceId, roomId, eventId, eventTime) {
+        return request.post({
+            method: 'POST',
+            uri: `http://${config.host}:${config.port}/${config.version}/message/${language}/${personality}`,
+            body: {
+                message,
+                sourceId,
+                roomId,
+                eventId,
+                eventTime
+            },
+            json: true
+        });
+    }
+}
